25c37d20a0ef47b48d8baf8c202a26b6c2912d73
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2023-11-30 13:44:44 +0800

    added bash script for install

--- install.sh
+++ install.sh
@@ -3,9 +3,12 @@
 # check if WM_PROJDIR has been set
 # check if the foam source file has been set
 
+
 if test -z "$WM_PROJECT_DIR"
 then 
+	openfoam_env_set=true
 	echo "openfoam env not set"
 else 
+	openfoam_env_set=false
 	echo "openfoam env set"
 fi

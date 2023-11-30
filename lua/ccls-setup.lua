f35efb04cc70b8dbdd3fd4e63e51730fae52812d
teddy0@archDesktop <tong011@e.ntu.edu.sg>
2023-11-30 10:32:11 +0800

    added ccls setup error if openfoam environment not set

--- lua/ccls-setup.lua
+++ lua/ccls-setup.lua
@@ -1,16 +1,34 @@
 -- test print
 -- print("hello from ccls")
-require("ccls").setup {
-    lsp = {
-        -- check :help vim.lsp.start for config options
-        server = {
-            name = "ccls", --String name
-            cmd = {"/usr/bin/ccls"}, -- point to your binary, has to be a table
-            args = {--[[Any args table]] },
-            offset_encoding = "utf-32", -- default value set by plugin
-            root_dir = vim.fs.dirname(vim.fs.find({ "compile_commands.json", ".git" }, { upward = true })[1]), -- or some other function that returns a string
-            --on_attach = your_func,
-            --capabilites = your_table/func
-        },
-    },
-}
+
+-- first i get the openfoam directory
+-- and check if it's set
+local openfoam_dir = vim.fn.expand('$WM_PROJECT_DIR')
+local openfoam_env_set = false
+
+if openfoam_dir == '$WM_PROJECT_DIR' then
+	openfoam_env_set = false
+else
+	openfoam_env_set = true
+end
+
+if openfoam_env_set then
+	print("openfoam environment set, setting up ccls openfoam server")
+	require("ccls").setup {
+		lsp = {
+			-- check :help vim.lsp.start for config options
+			server = {
+				name = "ccls", --String name
+				cmd = {"/usr/bin/ccls"}, -- point to your binary, has to be a table
+				args = {--[[Any args table]] },
+				offset_encoding = "utf-32", -- default value set by plugin
+				root_dir = vim.fs.dirname(vim.fs.find({ "compile_commands.json", ".git" }, { upward = true })[1]), -- or some other function that returns a string
+				--on_attach = your_func,
+				--capabilites = your_table/func
+			},
+		},
+	}
+else
+	error("openfoam environment not set, please source your openfoam etc/bashrc file")
+end
+
